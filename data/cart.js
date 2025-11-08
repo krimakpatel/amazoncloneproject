3fa84f885077dc19c868a633b1a0c49a9a938504
krimakpatel <krima6398.patel@gmail.com>
2025-11-08 14:19:33 -0500

    make delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -4,9 +4,11 @@ if (!cart) {
     cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
+    deliveryOptionId: '1'
 }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1,
+    deliveryOptionId: '2'
 }];
 }
 
@@ -27,7 +29,8 @@ export function addToCart (productId) {
         } else {
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: 1,
+                deliveryOptionId: '1'
             });
         }
     saveToStorage();
@@ -44,4 +47,16 @@ export function removeFromCart(productId) {
 
     cart = newCart;
     saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingitem;
+        cart.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+                matchingitem = cartItem;
+            }
+        });
+
+        matchingitem.deliveryOptionId = deliveryOptionId;
+        saveToStorage();
 }
