babfb7dd4f6e2e0b94d16e5e2c375f1327a2f2d7
krimakpatel <krima6398.patel@gmail.com>
2025-11-11 10:15:41 -0500

    Create oop version of cart

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
     cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -10,6 +14,7 @@ if (!cart) {
     quantity: 1,
     deliveryOptionId: '2'
 }];
+}
 }
 
 function saveToStorage() {
