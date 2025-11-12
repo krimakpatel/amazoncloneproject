1d59bf28be040d53bbc8e941a7b1efc1638f498d
krimakpatel <krima6398.patel@gmail.com>
2025-11-12 06:58:13 -0500

    last update

--- data/cart.js
+++ data/cart.js
@@ -65,3 +65,17 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
         matchingitem.deliveryOptionId = deliveryOptionId;
         saveToStorage();
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response);
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/cart");
+  xhr.send();
+}
